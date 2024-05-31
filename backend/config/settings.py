f094c0b37d8280ade2e170c79a255817cbd2e93f
princekumarofficial <x.prince.x@outlook.com>
2024-05-31 16:59:05 +0530

    Hotel Image field Deployed on Render .env Image on frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -63,6 +63,13 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
+#MEDIA
+# Actual directory user files go to
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+
+# URL used to access the media
+MEDIA_URL = '/media/'
+
 
 # Application definition
 
