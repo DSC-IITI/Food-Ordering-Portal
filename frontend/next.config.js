13948d5fc8c82fa9d2fd5bd57ac8ccdaa7978cca
princekumarofficial <x.prince.x@outlook.com>
2024-09-11 23:34:39 +0530

    fixes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,22 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
-    reactStrictMode: false,
-    images: {
-      domains: ["localhost"],
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback.fs = false;
-      }
-      return config;
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://food-ordering-portal-tjph.onrender.com/:path*',
+          },
+        ]
+      },
   };
-  
-  module.exports = nextConfig;
-  
