d78c3f95c1c9d5fb023fbfa92539597e3e99e5c1
princekumarofficial <x.prince.x@outlook.com>
2024-09-11 15:44:50 +0530

    update commands

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations api authentication business customer payment
+python manage.py makemigrations api business customer payment
 
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
